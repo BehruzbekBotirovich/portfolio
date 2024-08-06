5e0a7d0b364987a1e34daaaaf7a71282cbb7642b
BehruzbekBotirovich <behruzbekbotirovich@gmail.com>
2024-08-06 11:34:00 +0500

    Добавлен новый компонент Vue

---
+++ src/components/SliderImg.vue
@@ -0,0 +1,35 @@
+<template>
+    <a-carousel :after-change="onChange">
+        <div>
+            <h3>1</h3>
+        </div>
+        <div>
+            <h3>2</h3>
+        </div>
+        <div>
+            <h3>3</h3>
+        </div>
+        <div>
+            <h3>4</h3>
+        </div>
+    </a-carousel>
+</template>
+<script setup>
+const onChange = current => {
+    console.log(current);
+};
+</script>
+<style scoped>
+/* For demo */
+:deep(.slick-slide) {
+    text-align: center;
+    height: 160px;
+    line-height: 160px;
+    background: #364d79;
+    overflow: hidden;
+}
+
+:deep(.slick-slide h3) {
+    color: #fff;
+}
+</style>
